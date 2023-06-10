0a9039acaa64d4c3e4df947a0eae3690ac5de2e3
ArtyomGrigorev115 <artyomgrigorev115@gmail.com>
2023-06-10 19:24:55 +0700

    Создал RecyclerView и временный список заметок для тестирования

--- app/src/main/java/com/kurs2/mynotes/MainActivity.kt
+++ app/src/main/java/com/kurs2/mynotes/MainActivity.kt
@@ -2,10 +2,29 @@ package com.kurs2.mynotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    /*RecyclerView - отображает список заметок*/
+    private val recyclerView: RecyclerView by lazy { findViewById(R.id.recyclerViewNotes) }
+
+    /*Спосок заметок*/
+    private val notes: MutableList<Note> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*заметки в спсике заметок*/
+        notes.add( Note("Парикмахер", "Сделать прическу", "Понедельник", 2));
+        notes.add( Note("Баскетбол", "Игра со школьной командой", "Вторник", 3));
+        notes.add( Note("Магазин", "Купить новые джинсы", "Понедельник", 3));
+        notes.add( Note("Стоматолог", "Вылечить зубы", "Понедельник", 2));
+        notes.add( Note("Парикмахер", "Сделать прическу к выпускному", "Среда", 1));
+        notes.add( Note("Баскетбол", "Игра со школьной командой", "Вторник", 3));
+        notes.add( Note("Магазин", "Купить новые джинсы", "Понедельник", 3));
+
+
     }
 }
